c6df01481a9fae305d2796982dc43e2b0fb763f3
Yohan Spychala <yohan.spychala@adevinta.com>
2022-09-09 17:41:53 +0200

    Update gitconfig warning

--- bin/gitconfig-warning.sh
+++ bin/gitconfig-warning.sh
@@ -3,4 +3,4 @@
 RED='\033[0;33m'
 NC='\033[0m' # No Color
 
-echo -e "${RED}WARNING: .gitconfig has been changed. Be sure to not commit and push sensitive data.\n${NC}"
+echo -e "${RED}WARNING: .gitconfig has been changed. Be sure to not commit and push sensitive data. Use git add -p .gitconfig to select the changes to be staged.\n${NC}"
